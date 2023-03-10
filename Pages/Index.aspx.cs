3583bad85f88dc804249a231b3d65dd8c08c51de
Juan Espinoza <juanespinozamoreira@gmail.com>
2023-03-09 22:40:43 -0500

    se agrega ek proyecto

---
+++ Pages/Index.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ComponentModel;
+
+namespace CRUD.Pages
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        readonly SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conexion"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CargarTabla();
+        }
+
+        void CargarTabla()
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("sp_load", con); //creamos un objeto de tipo sqlCommand para acceder a los procedimientos almacenados. Recibe como paramentro el nombre del procedimiento y la variable del tipo conexion
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            gvusuarios.DataSource = dt;
+            gvusuarios.DataBind();
+            con.Close();
+            
+                 
+            }
+            catch (Win32Exception w)
+            {
+                Console.WriteLine(w.Message);
+                Console.WriteLine(w.ErrorCode.ToString());
+                Console.WriteLine(w.NativeErrorCode.ToString());
+                Console.WriteLine(w.StackTrace);
+                Console.WriteLine(w.Source);
+                Exception e = w.GetBaseException();
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        protected void BtnCreate_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Pages/CRUD.aspx?op=C");
+        }
+
+        protected void BtnRead_Click(object sender, EventArgs e)
+        {
+            string id;
+            Button BtnConsultar =(Button)sender;
+            GridViewRow selectedrow = (GridViewRow)BtnConsultar.NamingContainer;
+            id = selectedrow.Cells[1].Text;
+            Response.Redirect("~/Pages/CRUD.aspx?id=" + id+"&op=R");
+        }
+
+        protected void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            string id;
+            Button BtnConsultar = (Button)sender;
+            GridViewRow selectedrow = (GridViewRow)BtnConsultar.NamingContainer;
+            id = selectedrow.Cells[1].Text;
+            Response.Redirect("~/Pages/CRUD.aspx?id=" + id + "&op=U");
+        }
+
+        protected void BtnDelete_Click(object sender, EventArgs e)
+        {
+            string id;
+            Button BtnConsultar = (Button)sender;
+            GridViewRow selectedrow = (GridViewRow)BtnConsultar.NamingContainer;
+            id = selectedrow.Cells[1].Text;
+            Response.Redirect("~/Pages/CRUD.aspx?id=" + id + "&op=D");
+        }
+    }
+}
